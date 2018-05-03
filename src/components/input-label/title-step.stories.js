f896db37b4f6aff7f6c9e925d03565c995062e5c
Clement Fradet Normand <clement.fradet.normand@gmail.com>
2018-05-03 14:37:12 +0200

    add input label

---
+++ src/components/input-label/title-step.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
+import centered from '@storybook/addon-centered';
+
+import InputLabel from './';
+
+
+storiesOf('Input.Label', module)
+
+    .addDecorator(backgrounds([
+        { name: 'header', value: 'white', default: true },
+    ]))
+    .addDecorator(centered)
+
+    .add('normal', () => (
+        <InputLabel>
+            Aller
+        </InputLabel>
+    ))
+
+    .add('important', () => (
+        <InputLabel isImportant>
+            Retour*
+        </InputLabel>
+    ))
+
+    .add('with input id', () => (
+        <InputLabel
+            inputId="StartInput"
+        >
+            Aller
+        </InputLabel>
+    ));
+
