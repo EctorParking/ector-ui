8733aaf55c8c38ecd6d07387bfb20add78190d94
Julien Fradin <julien@frad.in>
2018-06-13 11:20:09 +0200

    Add ColorPicker component

---
+++ src/components/ColorPicker/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import s from './ColorPicker.css';
+
+const ColorPicker = ({
+    colorsList, onSelect,
+}) => (
+    <div className={s.colorPicker}>
+        {colorsList.map(color => (
+            <button
+                className={`${s.badge} ${color.hexadecimalCode === 'transparent' ? s.transparent : ''}`}
+                style={{ backgroundColor: color.hexadecimalCode }}
+                title={color.label}
+                onClick={() => onSelect(color)}
+            />
+        ))}
+    </div>
+);
+
+ColorPicker.propTypes = {
+    colorsList: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string,
+        hexadecimalCode: PropTypes.string,
+    })).isRequired,
+    onSelect: PropTypes.func.isRequired,
+};
+
+export default ColorPicker;
