ffcebc034f2337ea8eda3dfe2d9d2af3bbed6120
Julien Fradin <julien@frad.in>
2018-06-27 09:28:58 +0200

    Add PhoneInput and Select components

---
+++ src/components/PhoneInput/PhoneInput.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
+import centered from '@storybook/addon-centered';
+
+import PhoneInput from './';
+
+storiesOf('PhoneInput', module)
+
+    .addDecorator(backgrounds([
+        { name: 'header', value: 'white', default: true },
+    ]))
+    .addDecorator(centered)
+
+    .add('basic', () => (
+        <PhoneInput />
+    ));
---
+++ src/components/Select/SelectValueType.js
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+
+export default PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+    image: PropTypes.string,
+});
