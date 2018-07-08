0bcdbdbc93330672d21b7164f1029a561d7dd305
Julien Fradin <julien@frad.in>
2018-07-08 11:06:24 +0200

    Add ApplicationCard component

---
+++ src/components/ApplicationCard/ApplicationCard.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
+import centered from '@storybook/addon-centered';
+
+import ApplicationCard from './';
+
+const urls = {
+    googlePlay: 'http://www.google.fr',
+    appStore: 'http://www.apple.com/fr',
+};
+
+storiesOf('ApplicationCard', module)
+
+    .addDecorator(backgrounds([
+        { name: 'header', value: 'white', default: true },
+    ]))
+    .addDecorator(centered)
+
+    .add('normal', () => (
+        <ApplicationCard
+            title="Téléchargez l’application"
+            description="Gérez vos réservations plus facilement depuis l’application."
+            urls={urls}
+        />
+    ));
