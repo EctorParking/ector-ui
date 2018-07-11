fbcce04905f89d1dbf719a17ca17c9cbd2217753
Jalil Dabbech <jalil.dabbech@ectorparking.com>
2018-07-11 17:59:43 +0200

    Fix components export to import them separatly in web app

--- src/components/index.js
+++ src/components/index.js
@@ -1,35 +1,16 @@
-import CardTitle from './card-title';
-import TitleStep from './title-step';
-import Input from './input';
-import InputLabel from './input-label';
-import ColorPicker from './ColorPicker';
-import CarCard from './CarCard';
-import ActionLink from './ActionLink';
-import Alert from './Alert';
-import Select from './Select';
-import PhoneInput from './PhoneInput';
-import PricingSummary from './PricingSummary';
-import ReferralCard from './ReferralCard';
-import BookingSteps from './BookingSteps';
-import Title from './Title';
-import Subtitle from './Subtitle';
-import ContactForm from './ContactForm';
-
-export default {
-    CardTitle,
-    TitleStep,
-    Input,
-    InputLabel,
-    ColorPicker,
-    CarCard,
-    ActionLink,
-    Alert,
-    Select,
-    PhoneInput,
-    PricingSummary,
-    ReferralCard,
-    BookingSteps,
-    Title,
-    Subtitle,
-    ContactForm,
-};
+export { default as CardTitle } from './CardTitle';
+export { default as TitleStep } from './TitleStep';
+export { default as Input } from './Input';
+export { default as InputLabel } from './InputLabel';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Card } from './Card';
+export { default as CarCard } from './CarCard';
+export { default as ActionLink } from './ActionLink';
+export { default as Alert } from './Alert';
+export { default as Select } from './Select';
+export { default as PhoneInput } from './PhoneInput';
+export { default as ReferralCard } from './ReferralCard';
+export { default as BookingSteps } from './BookingSteps';
+export { default as Title } from './Title';
+export { default as Subtitle } from './Subtitle';
+export { default as ContactForm } from './ContactForm';
