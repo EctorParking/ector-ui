b3a2089fef1e0a3a9b2704b166a35d46d37ca1a8
Julien Fradin <julien@frad.in>
2018-07-11 15:34:18 +0200

    Add BookingCard component

---
+++ src/components/BookingCard/BookingCard.stories.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
+import centered from '@storybook/addon-centered';
+
+import BookingCard from './';
+import ActionLink from '../ActionLink';
+import LinkUnderlined from '../link-underlined';
+
+const bookingStepsTexts = {
+    pickup: 'Prise en charge',
+    return: 'Restitution',
+};
+
+const options = [
+    { label: 'Parking + Voiturier', price: 64 },
+    { label: 'Parking couvert', price: 9 },
+    { label: 'Cagnotte', price: -3 },
+];
+
+const pricingSummaryTexts = {
+    addOption: 'Ajouter une option',
+    total: 'Total TTC',
+};
+
+const leftActions = [
+    <ActionLink label="Modifier" />,
+    <ActionLink label="Annuler" />,
+];
+
+const rightAction = <LinkUnderlined>Télécharger la facture</LinkUnderlined>;
+
+storiesOf('BookingCard', module)
+
+    .addDecorator(backgrounds([
+        { name: 'header', value: 'white', default: true },
+    ]))
+    .addDecorator(centered)
+
+    .add('normal', () => (
+        <BookingCard
+            fromSpot="Paris Gare de l’Est"
+            toSpot="Paris Gare de l’Est"
+            startAt="Ven. 20 avril à 15:00"
+            endAt="Dim. 22 avril à 15:00"
+            bookingStepsTexts={bookingStepsTexts}
+            options={options}
+            pricingSummaryTexts={pricingSummaryTexts}
+            leftActions={leftActions}
+            rightAction={rightAction}
+        />
+    ));
