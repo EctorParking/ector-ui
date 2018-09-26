fc62d6d8f40b3cbd6d9b0996bc182de38a97ff54
Jalil Dabbech <jalil.dabbech@ectorparking.com>
2018-09-26 17:05:16 +0200

    Improve title component to override html element on it

--- src/components/Title/Title.stories.js
+++ src/components/Title/Title.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import backgrounds from '@storybook/addon-backgrounds';
 import centered from '@storybook/addon-centered';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 import Title from './';
 
@@ -17,6 +17,7 @@ storiesOf('Title', module)
   .add('with knobs', () => {
     const props = {
       label: text('Label', 'Récapitulatif de réservation'),
+      htmlElement: select('Element HTML', ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], 'h1'),
     };
 
     return <Title {...props} />;
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Title.css';
 
-const Title = ({ label, className }) => (
+const Title = ({ label, className, htmlElement }) => (
   <div className={`${s.title} ${className}`}>
-    <h1>{label}</h1>
-    <hr />
+    {React.createElement(htmlElement, { className: s.element }, label)}
+    <hr className={s.horizontalRule} />
   </div>
 );
 
 Title.defaultProps = {
   className: '',
+  htmlElement: 'h1',
 };
 
 Title.propTypes = {
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
+  htmlElement: PropTypes.string,
 };
 
 export default Title;
