f28c7fa21c23dea99ffb04e1577b39a50842cc45
Antoine Gourtay <antoine.gourtay@hetic.net>
2018-10-24 11:39:31 +0200

    Finished reward card desig

--- src/components/RewardCard/RewardCard.stories.js
+++ src/components/RewardCard/RewardCard.stories.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import backgrounds from '@storybook/addon-backgrounds';
 import centered from '@storybook/addon-centered';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
-import Card from './';
+import { withKnobs } from '@storybook/addon-knobs';
 
+import RewardCard from './';
+
+const rewardText = 'de cagnotte fidélité grâce à votre statut platinum';
 
 storiesOf('RewardCard', module)
 
@@ -15,27 +16,25 @@ storiesOf('RewardCard', module)
   .addDecorator(centered)
   .addDecorator(withKnobs)
 
-  .add('with knobs', () => {
-    const props = {
-      isSelected: boolean('Selected', false),
-      FooterChildren: text('The Footer Node'),
-      onClick: action('clicked'),
-    };
-
-    return (
-      <Card {...props} headerImage="../../images/poolRewardLarge.png">
+  .add('with knobs', () => (
+    <RewardCard
+      headerImage="../../images/poolRewardLarge.png"
+      rewardText={rewardText}
+      rewardTooltip="Ceci est un tootip"
+      rewardValue="+1,95€"
+      footerButtonText="Choisir"
+      footerOnClick={() => {}}
+    >
                 Content
-      </Card>
-    );
-  })
-  .add('with label', () => {
-    const props = {
-      isSelected: boolean('Selected', false),
-    };
-
-    return (
-      <Card {...props} headerImage="../../images/flyingBlueRewardLarge.png">
-                Content
-      </Card>
-    );
-  });
+    </RewardCard>
+  ))
+  .add('with label', () => (
+    <RewardCard
+      headerImage="../../images/flyingBlueRewardLarge.png"
+      rewardText={rewardText}
+      rewardTooltip="Ceci est un tootip"
+      rewardValue="+60 Miles"
+      footerButtonText="Choisir"
+      footerOnClick={() => {}}
+    />
+  ));
--- src/components/RewardCard/index.js
+++ src/components/RewardCard/index.js
@@ -3,21 +3,55 @@ import PropTypes from 'prop-types';
 import Card from '../Card';
 import s from './RewardCard.css';
 import RewardCardHeader from './RewardCardHeader';
+import RewardCardContent from './RewardCardContent';
+import RewardCardFooter from './RewardCardFooter';
 
 const RewardCard = ({
   headerImage,
+  rewardValue,
+  rewardText,
+  rewardTooltip,
+  footerButtonText,
+  footerButtonColor,
+  footerOnClick,
 }) => {
   const header = <RewardCardHeader headerImage={headerImage} />;
+  const footer = (
+    <RewardCardFooter
+      onClick={footerOnClick}
+      buttonText={footerButtonText}
+      buttonColor={footerButtonColor}
+    />);
 
-  return (<Card
-    className={s.card}
-    HeaderChildren={header}
-  />
+  return (
+    <Card
+      className={s.card}
+      HeaderChildren={header}
+      FooterChildren={footer}
+      footerClassName={s.footer}
+      contentClassName={s.content}
+    >
+      <RewardCardContent
+        rewardTooltip={rewardTooltip}
+        rewardText={rewardText}
+        rewardValue={rewardValue}
+      />
+    </Card>
   );
 };
 
 RewardCard.propTypes = {
   headerImage: PropTypes.string.isRequired,
+  rewardValue: PropTypes.string.isRequired,
+  rewardText: PropTypes.string.isRequired,
+  rewardTooltip: PropTypes.string.isRequired,
+  footerButtonText: PropTypes.string.isRequired,
+  footerButtonColor: PropTypes.string,
+  footerOnClick: PropTypes.func.isRequired,
+};
+
+RewardCard.defaultProps = {
+  footerButtonColor: '#ffcd02',
 };
 
 
