701a4dd3757e1b352172bcb33ca847e2d255653b
Gvlaxy <g.galas@live.fr>
2019-01-23 12:34:10 +0100

    Add values, errors and onChange handling to RegistrationForm

--- src/components/RegistrationForm/RegistrationFormTextTypes.js
+++ src/components/RegistrationForm/RegistrationFormTextTypes.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 export default PropTypes.shape({
   firstChoice: PropTypes.string,
   secondChoice: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  postalCode: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  confirmPassword: PropTypes.string,
+  title: PropTypes.string,
 });
 
 const defaultTexts = {
@@ -15,6 +23,7 @@ const defaultTexts = {
   email: 'Email',
   password: 'Mot de passe',
   confirmPassword: 'Confirmation de mot de passe',
+  title: 'Civilité',
 };
 
 const radioDefaultValues = {
@@ -22,4 +31,80 @@ const radioDefaultValues = {
   secondChoice: 'Mme',
 };
 
-export { defaultTexts, radioDefaultValues };
+const OnChangesRegistrationFormType = {
+  gender: PropTypes.func,
+  firstName: PropTypes.func,
+  lastName: PropTypes.func,
+  postalCode: PropTypes.func,
+  email: PropTypes.func,
+  phone: PropTypes.func,
+  password: PropTypes.func,
+  confirmPassword: PropTypes.func,
+};
+
+const defaultOnChanges = {
+  gender: () => {},
+  firstName: () => {},
+  lastName: () => {},
+  postalCode: () => {},
+  email: () => {},
+  phone: () => {},
+  password: () => {},
+  confirmPassword: () => {},
+};
+
+const ValuesType = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  postalCode: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  confirmPassword: PropTypes.string,
+  title: PropTypes.string,
+};
+
+const ErrorsType = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  postalCode: PropTypes.string,
+  phone: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  confirmPassword: PropTypes.string,
+  title: PropTypes.string,
+};
+
+const defaultValue = {
+  firstName: '',
+  lastName: '',
+  postalCode: '',
+  phone: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  title: '',
+};
+
+const defaultErrors = {
+  firstName: null,
+  lastName: null,
+  postalCode: null,
+  phone: null,
+  email: null,
+  password: null,
+  confirmPassword: null,
+  title: null,
+};
+
+
+export {
+  defaultTexts,
+  radioDefaultValues,
+  OnChangesRegistrationFormType,
+  defaultOnChanges,
+  ValuesType,
+  ErrorsType,
+  defaultValue,
+  defaultErrors,
+};
