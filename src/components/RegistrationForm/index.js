5a3bda4e0ae52a498d2076c7fe29b0d3bbb77dfd
Gvlaxy <g.galas@live.fr>
2019-01-24 13:58:48 +0100

    Add missing props error in RegisterForm

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -63,6 +63,7 @@ const RegistrationForm = ({
           label={texts.phone}
           onChange={onChanges.phone}
           value={values.phone}
+          error={errors.phone}
           mandatory
         />
         <InputLabel
