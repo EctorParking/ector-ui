9f210995f23ba387f2de757170b07ee50e2f2e88
Jalil Dabbech <jalil.dabbech@ectorparking.com>
2019-04-01 10:31:00 +0200

    Use initialize.js to import global styles

---
+++ src/initialize.js
@@ -0,0 +1,8 @@
+import 'normalize.css/normalize.css';
+import './style/font.global.css';
+import './style/index.global.css';
+import 'react-dates/lib/css/_datepicker.css';
+import './style/react-dates.global.css';
+import './style/icomoon.global.css';
+
+export default {};
