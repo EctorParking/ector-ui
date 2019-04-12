89f37de6943aeac112429e292a93ff8bad4f2b94
Jalil Dabbech <jalil.dabbech@ectorparking.com>
2019-04-12 10:53:29 +0200

    Fix icomoon file structure

--- src/initialize.js
+++ src/initialize.js
@@ -3,6 +3,6 @@ import './style/font.global.css';
 import './style/index.global.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './style/react-dates.global.css';
-import './style/icomoon.global.css';
+import './assets/icomoon/icomoon.global.css';
 
 export default {};
